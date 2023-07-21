f74aa29ecf72c7aa2e9da170acb546e4c037b7ab
sipangkar1d <daniel222mu@gmail.com>
2023-07-21 21:48:22 +0700

    Initial commit

---
+++ src/test/java/com/enigma/procurement/ProcurementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.procurement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
